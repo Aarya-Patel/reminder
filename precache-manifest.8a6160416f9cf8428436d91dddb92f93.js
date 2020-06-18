36454a92c44f34307575af08470cf6c4e6ffdd83
aaryapatel <aarya.patel@mail.utoronto.ca>
2020-06-18 14:52:07 -0400

    Updates

---
+++ precache-manifest.8a6160416f9cf8428436d91dddb92f93.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4173eb2dfed70bd9482e47edc00f4b0f",
+    "url": "/Reminder/index.html"
+  },
+  {
+    "revision": "1091a3e9743317cc6bcc",
+    "url": "/Reminder/static/css/main.fdd6e3c1.chunk.css"
+  },
+  {
+    "revision": "832792811bf5dc26640f",
+    "url": "/Reminder/static/js/2.530fdd86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reminder/static/js/2.530fdd86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1091a3e9743317cc6bcc",
+    "url": "/Reminder/static/js/main.d7b04d99.chunk.js"
+  },
+  {
+    "revision": "a62a4a5bad4c36e1eb64",
+    "url": "/Reminder/static/js/runtime-main.ac23c751.js"
+  },
+  {
+    "revision": "3e6bd45ad38d784e5db88db8371b3a17",
+    "url": "/Reminder/static/media/check.3e6bd45a.svg"
+  },
+  {
+    "revision": "7d777571edb16d6139458a55377161e8",
+    "url": "/Reminder/static/media/complete.7d777571.svg"
+  },
+  {
+    "revision": "411a5cdecde06c25cb9819e5ad7f8360",
+    "url": "/Reminder/static/media/delete.411a5cde.svg"
+  },
+  {
+    "revision": "5a835eb984166e0ad9d7357007a64c7d",
+    "url": "/Reminder/static/media/star.5a835eb9.svg"
+  },
+  {
+    "revision": "14f40330e2a034acf8145a2bb3352a4f",
+    "url": "/Reminder/static/media/toggle.14f40330.svg"
+  }
+]);
